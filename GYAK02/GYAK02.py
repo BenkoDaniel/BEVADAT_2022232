707c94b8466f20bf0fc31d95123e45e72495d690
BenkoDaniel <benko.dani01@gmail.com>
2023-03-07 13:11:01 +0100

    Error fixes

--- GYAK02/GYAK02.py
+++ GYAK02/GYAK02.py
@@ -1,6 +1,9 @@
 import numpy as np
+
+
 #FONTOS!!!
 # CSAK OTT LEHET HASZNÁLNI FOR LOOP-OT AHOL A FELADAT KÜLÖN KÉRI!
+
 
 #Készíts egy függvényt ami létre hoz egy nullákkal teli numpy array-t.
 #Paraméterei: mérete (tupel-ként), default mérete pedig legyen egy (2,2)
@@ -8,7 +11,8 @@ import numpy as np
 #Ki: [[0,0],[0,0]]
 #create_array()
 
-def create_array(shape=(2,2)):
+
+def create_array(shape=(2, 2)):
     return np.zeros(shape, dtype=tuple)
 
 
@@ -34,6 +38,7 @@ def do_transpose(input_matrix):
 # Ki: [0.12, 0.17]
 # round_array()
 
+
 def round_array(input_array, n=2):
     return np.round(input_array, n)
 
@@ -44,8 +49,7 @@ def round_array(input_array, n=2):
 # bool_array()
 
 def bool_array(input_array):
-    return np.array.astype(input_array, dtype=bool)
-
+    return np.astype(input_array, dtype=bool)
 
 
 # Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 1 - False-ra az 0 True-ra cserélni
@@ -53,15 +57,16 @@ def bool_array(input_array):
 # Ki: [[ True False False], [ True  True  True], [False False False]]
 # invert_bool_array()
 
+
 def invert_bool_array(input_array):
     return np.invert(np.astype(input_array, dtype=bool))
-
 
 
 # Készíts egy olyan függvényt ami a paraméterként kapott array-t kilapítja
 # Be: [[1,2], [3,4]]
 # Ki: [1,2,3,4]
 # flatten()
+
 
 def flatten(input_array):
     return input_array.reshape(-1)
