fb9b4d40db265365392b268778e473f4d7993386
BenkoDaniel <benko.dani01@gmail.com>
2023-04-04 17:02:07 +0200

    homework progress

--- HAZI/HAZI06/NJCleaner.py
+++ HAZI/HAZI06/NJCleaner.py
@@ -6,42 +6,60 @@ class NJCleaner:
 
 
     def order_by_scheduled_time(self):
-        new_df = self.data.copy()
-        return new_df.sort_values(by=['scheduled_time'], ascending=True)
+        return self.data.sort_values(by=['scheduled_time'], ascending=True)
 
     def drop_columns_and_nan(self):
-        new_df = self.data.copy()
         labels = ['from', 'to']
-        new_df = new_df.drop(labels=labels)
-        return new_df.dropna()
+        self.data = self.data.drop(labels=labels)
+        return self.data.dropna()
 
 
     def convert_date_to_day(self):
-        new_df = self.data.copy()
-        new_df['day'] = new_df['date']._convert()
-        new_df.drop('date')
+        self.data['day'] = self.data['date'].strftime()
+        self.data.drop('date')
+        return self.data
 
     def convert_scheduled_time_to_part_of_the_day(self):
-        new_df = self.data.copy()
         helper = []
-        for x in new_df['scheduled_time']:
-            if(x > 4 & x < 8):
-                helper.append('early morning')
-            if(x > 8 & x < 12):
-                helper.append('morning')
-            if(x > 12 & x < 16):
-                helper.append('afternoon')
-            if(x > 16 & x < 20):
-                helper.append('evening')
-            if(x > 20 & x < 24):
-                helper.append('night')
-            else:
+        for x in pd.to_datetime(self.data['scheduled_time']).dt.hour():
+            if(x < 4):
                 helper.append('late night')
+            if(x < 8):
+                helper.append('early_morning')
+            if(x <12):
+                helper.append('morning')
+            if(x < 16):
+                helper.append('afternoon')
+            if(x < 20):
+                helper.append('evening')
+            else:
+                helper.append('night')
 
-        new_df['part_of_the_day'] = helper
-        return new_df
+        self.data['part_of_the_day'] = helper
+        return self.data
 
-    #def convert_delay(self):
+    def convert_delay(self):
+        self.data['delay'] = 0 if (self.data['delay_minutes'] < 5) else 1
+        return self.data
+
+    def drop_unnecessary_columns(self):
+        labels = ['train_id', 'scheduled_time', 'actual_time', 'delay_minutes']
+        return self.data.drop(labels=labels)
+
+    def save_first_60k(self, save_path):
+        new_df = self.data[:60000]
+        new_df.to_csv(save_path)
+
+    def prep_df(self, save_path='data/NJ.csv'):
+        self.data = self.order_by_scheduled_time()
+        self.data = self.drop_columns_and_nan()
+        self.data = self.convert_date_to_day()
+        self.data = self.convert_scheduled_time_to_part_of_the_day()
+        self.data = self.convert_delay()
+        self.data = self.drop_unnecessary_columns()
+        self.save_first_60k(save_path)
+
+
 
 
 
